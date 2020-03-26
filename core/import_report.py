2acc95fb923abb221243e862cfcd4b293321ee2b
Slava Skvortsov <kondor1995@mail.ru>
2020-03-26 16:23:45 +0100

    add limits

--- core/import_report.py
+++ core/import_report.py
@@ -1,10 +1,11 @@
 import csv
 from datetime import date, datetime
-from typing import List, TextIO
+from typing import List, TextIO, Set
 
+from django.db.models import Count
 from django.utils.functional import cached_property
 
-from core.constants import ENCOUNTERS, ITEM_ID_TO_BONUS_EP, SPELL_TO_ITEM_MAP, TANKS
+from core.constants import ENCOUNTERS, ITEM_ID_TO_BONUS_EP, SPELL_TO_ITEM_MAP, TANKS, LIMITS
 from core.utils import parse_datetime_str
 from extra_ep.models import Combat, ItemConsumption, Player, Report
 
@@ -48,7 +49,7 @@ class ReportImporter:
                 combat.ended = time
                 combat.save()
                 combat = self._create_unknown_combat()
-            elif combat and event == 'SPELL_CAST_SUCCESS':
+            elif event == 'SPELL_CAST_SUCCESS':
                 self._make_item_consumption(row, combat)
 
         if combat and combat.encounter == self.UNKNOWN_COMBAT_NAME:
@@ -56,6 +57,44 @@ class ReportImporter:
 
         self._report.raid_name = ', '.join(sorted(raids))
         self._report.save()
+        self._post_analyze_report(raids)
+
+    def _post_analyze_report(self, raids: Set[str]) -> None:
+        limits = {}
+        for raid in raids:
+            new_limits = LIMITS.get(raid)
+            if new_limits is None:
+                continue
+
+            for item_id, amount in new_limits.items():
+                if item_id in limits:
+                    limits[item_id] += amount
+                else:
+                    limits[item_id] = amount
+
+        for item_id, amount in limits.items():
+            qs = ItemConsumption.objects.filter(
+                combat__report_id=self.report_id,
+                item_id=item_id,
+            ).order_by().values(
+                'player_id',
+            ).annotate(
+                amount=Count('item_id'),
+            ).filter(
+                amount__gt=amount,
+            ).values_list(
+                'player_id', 'amount',
+            )
+
+            for player_id, total_amount in qs:
+                item_consumptions_qs = ItemConsumption.objects.filter(
+                    combat__report_id=self.report_id,
+                    player_id=player_id,
+                    item_id=item_id,
+                ).order_by('-id')[:total_amount - amount]
+                for item_consumption in item_consumptions_qs:
+                    item_consumption.ep = 0
+                    item_consumption.save()
 
     def _create_unknown_combat(self) -> Combat:
         return Combat.objects.create(
