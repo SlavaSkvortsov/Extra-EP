920536c14232b13a6509e0b2648709f5ae862a82
Slava Skvortsov <kondor1995@mail.ru>
2020-08-26 17:37:44 +0200

    Disable nav. sidebar

--- urls.py
+++ urls.py
@@ -9,3 +9,6 @@ urlpatterns = [
     path('accounts/', include('django_registration.backends.one_step.urls')),
     path('accounts/', include(('django.contrib.auth.urls', 'auth'), namespace='auth')),
 ]
+
+
+admin.site.enable_nav_sidebar = False
